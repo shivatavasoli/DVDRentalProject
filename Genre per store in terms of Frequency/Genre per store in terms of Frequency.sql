ccef94bb1d73188dbd85f3f6f4df65c2cf4a39f7
shivatavasoli <88995558+shivatavasoli@users.noreply.github.com>
2023-10-20 11:26:39 -0400

    Update Genre per store in terms of Frequency.sql

--- Genre per store in terms of Frequency/Genre per store in terms of Frequency.sql
+++ Genre per store in terms of Frequency/Genre per store in terms of Frequency.sql
@@ -1,16 +1,25 @@
-with c as (
-Select store.store_id,
-film_category.category_id,
-category.name genre, 
-count(inventory.film_id) as frequency,
-sum(payment.amount) revenue
-From store
- Join inventory On inventory.store_id = store.store_id
- Join rental On rental.inventory_id = inventory.inventory_id 
- Join payment On payment.rental_id = rental.rental_id
- Join film_category On film_category.film_id = inventory.film_id Inner Join category On film_category.category_id = category.category_id
-group by store.store_id,film_category.category_id, category.name
-order by frequency desc
-)
-select c.store_id, c.name category_name, max(c.frequency) frequency,sum(c.revenue) total_revenue from c
-group by c.store_id, c.category_id, c.name order by max(c.frequency) desc;
+WITH C AS
+	(SELECT STORE.STORE_ID,
+			FILM_CATEGORY.CATEGORY_ID,
+			CATEGORY.NAME GENRE,
+			COUNT(INVENTORY.FILM_ID) AS FREQUENCY,
+			SUM(PAYMENT.AMOUNT) REVENUE
+		FROM STORE
+		JOIN INVENTORY ON INVENTORY.STORE_ID = STORE.STORE_ID
+		JOIN RENTAL ON RENTAL.INVENTORY_ID = INVENTORY.INVENTORY_ID
+		JOIN PAYMENT ON PAYMENT.RENTAL_ID = RENTAL.RENTAL_ID
+		JOIN FILM_CATEGORY ON FILM_CATEGORY.FILM_ID = INVENTORY.FILM_ID
+		INNER JOIN CATEGORY ON FILM_CATEGORY.CATEGORY_ID = CATEGORY.CATEGORY_ID
+		GROUP BY STORE.STORE_ID,
+			FILM_CATEGORY.CATEGORY_ID,
+			CATEGORY.NAME
+		ORDER BY FREQUENCY DESC)
+SELECT C.STORE_ID,
+	C.NAME CATEGORY_NAME,
+	MAX(C.FREQUENCY) FREQUENCY,
+	SUM(C.REVENUE) TOTAL_REVENUE
+FROM C
+GROUP BY C.STORE_ID,
+	C.CATEGORY_ID,
+	C.NAME
+ORDER BY MAX(C.FREQUENCY) DESC;
