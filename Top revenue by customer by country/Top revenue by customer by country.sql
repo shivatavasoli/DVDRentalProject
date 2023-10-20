1a738faea4223a9a218cdd486064a6bd464ccb99
shivatavasoli <88995558+shivatavasoli@users.noreply.github.com>
2023-10-20 06:31:56 -0400

    Create Top revenue by customer by country.sql

---
+++ Top revenue by customer by country/Top revenue by customer by country.sql
@@ -0,0 +1,16 @@
+
+select 
+c.country,
+max(cs.first_name) AS Name, 
+max(cs.email) AS Email , 
+max(ad.address) address, 
+max(ad.district) district, 
+max(ci.city) city, 
+sum(p.amount) AS Sales
+from country c
+join city ci using (country_id)
+join address ad on ci.city_id = ad.city_id
+join customer cs on cs.address_id = ad.address_id
+join payment p on cs.customer_id = p.customer_id
+group by country
+order by Sales desc
